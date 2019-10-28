6134f5f3f3f5bd3bceb5c0becba6d082e975819f
bobojoker <790268016@qq.com>
2019-10-28 23:32:37 +0800

    .NetCore 控制台
    类似helloworld的测试程序，之后会添加设计模式以及开发原则内容。

---
+++ ConsoleApp1/SignIn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class SignIn
+    {
+        public bool Sumbit(string content) {
+            if (!string.IsNullOrEmpty(content))
+            {
+                Console.WriteLine("you put in:{0}", content);
+                return true;
+            }
+            else return false;
+        }
+    }
+}
